50e52fca8e7b0b14a04336c433df96aae17a9d60
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-11-26 18:48:59 -0800

    code to set the frequency

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BG7TBL-RF-USB
-138MHz-4.4GHz 4.4G USB SMA Source/Signal Generator/Simple Spectrum Analyzer
+
+138MHz-4.4GHz USB RF Source Signal Generator / Simple Spectrum Analyzer
+
+![PCB](pcb.png)
+
+
+## References
+
+  * https://sigrok.org/wiki/BG7TBL
---
+++ bin/set.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const SerialPort = require('serialport');
+
+const c = {
+  start: String.fromCharCode(0x8f)
+};
+
+var port = new SerialPort('/dev/ttyUSB0', {
+  baudRate: 57600
+});
+
+port.on('error', err => console.log(err));
+
+port.on('data', data => console.log(data));
+
+port.on('open', () => {
+
+  // check firmware version
+  port.write(
+    (c.start + 'v'),
+    err => { if (err) { console.log(err.message); } }
+  );
+
+  // set frequency
+  // f013300000 = MIN
+  // f220000000 = 2.2e9
+  // f340000000 = 1.7e9?
+  port.write(
+    (c.start + 'f240000000'),
+    err => { if (err) { console.log(err.message); } }
+  )
+
+});
