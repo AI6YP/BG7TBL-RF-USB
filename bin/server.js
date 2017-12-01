486f9a18a189a2e029462b9e6fb4b2ac68f51bb2
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-11-30 23:41:45 -0800

    serving public folder

--- bin/server.js
+++ bin/server.js
@@ -8,25 +8,20 @@ const http = require('http');
 const url = require('url');
 const WebSocket = require('ws');
 const portfinder = require('portfinder');
+const ip = require('ip');
 
 var port = new SerialPort('/dev/ttyUSB0', {
   baudRate: 57600
 });
 
 const app = express();
-
-app.use(function (req, res) {
-  res.send({ msg: "hello" });
-});
-
+app.use(express.static('public'));
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws, req) {
   const location = url.parse(req.url, true);
   let tail;
-  // You might use location.query.access_token to authenticate or share sessions
-  // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
 
   port.on('error', err => console.log(err));
 
@@ -69,7 +64,7 @@ port.on('open', () => {
       throw err;
     }
     server.listen(port, function listening() {
-      console.log('Listening on %d', server.address().port);
+      console.log('(Ctrl + Click) on:\nhttp://' + ip.address() + ':' + server.address().port + '/');
     });
   });
 
