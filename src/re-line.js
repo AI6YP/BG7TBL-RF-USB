b2ccc12ae1f3e58a756a78815db46bda1d02c9ab
drom <alex.drom@gmail.com>
2018-05-22 12:16:55 -0700

    fixed 1 byte plot overflow

--- src/re-line.js
+++ src/re-line.js
@@ -4,14 +4,18 @@
     frequency response and the maximum line plots
 */
 
+const readInt16LEy = (arr, index) =>
+    499 - (arr[2 * index] + (arr[2 * index + 1] << 8));
+
 module.exports = function ($) {
     return function Line (props) {
         if (props.tail) {
-            const arr = Uint16Array.from(props.tail.data);
             const xs = props.width / props.samples / 2;
-            let p1d = 'M0 ' + (499 - arr[0]);
-            for (let i = 2; i < (arr.length / 2); i += 1) {
-                p1d += 'L' + (xs * i) + ' ' + (499 - arr[2 * i]);
+            const arr = props.tail.data;
+            const len = arr.length / 2;
+            let p1d = 'M0 ' + readInt16LEy(arr, 0);
+            for (let i = 2; i < len; i += 1) {
+                p1d += 'L' + (xs * i) + ' ' + readInt16LEy(arr, i);
             }
             // const p1dmax = props.p1dmax.reduce((res, e, i) => {
             //     if (i > 0) {
